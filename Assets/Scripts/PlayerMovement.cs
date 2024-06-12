22da21393e593a83173ad7027ef62697a4fa82a0
Иван Вячеславович Нургазин <ivan.nurgazin@yandex.ru>
2024-06-13 00:47:25 +0700

    Доработка управления персонажем

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,15 +12,19 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float mouseSensivity;
     [SerializeField] float health;
     [SerializeField] Camera gameCamera;
+    [SerializeField] Vector3 position;
 
     private Vector3 _moveDirection;
     private CharacterController _characterController;
+    private PlayerInput _playerInput;
     private Vector2 _mouseMove;
     private float xRotation = 0f;
 
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
+        _playerInput = GetComponent<PlayerInput>();
+        _playerInput.actions.Enable();
         Cursor.lockState = CursorLockMode.Locked;
         health = 100f;
     }
@@ -28,18 +32,21 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+        LookAround();
+    }
+    
+
+    public void Move()
+    {
+        Vector2 moveDirection = _playerInput.actions.FindAction("Movement").ReadValue<Vector2>();
+        _moveDirection = (moveDirection.y * transform.forward) + (moveDirection.x * transform.right);
         _characterController.Move(_moveDirection * moveSpeed * Time.deltaTime);
     }
 
-    public void OnMove(InputAction.CallbackContext context)
+    public void LookAround()
     {
-        Vector2 moveDirection = context.ReadValue<Vector2>();
-        _moveDirection = (moveDirection.y * transform.forward) + (moveDirection.x * transform.right);
-    }
-
-    public void OnVerticalCameraMove(InputAction.CallbackContext context)
-    {
-        _mouseMove = context.ReadValue<Vector2>();
+        _mouseMove = _playerInput.actions.FindAction("LookAround").ReadValue<Vector2>();
 
         float mouseX = _mouseMove.x * mouseSensivity * Time.deltaTime;
         float mouseY = _mouseMove.y * mouseSensivity * Time.deltaTime;
@@ -47,8 +54,8 @@ public class PlayerMovement : MonoBehaviour
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -40, 40);
 
-        gameCamera.transform.rotation = Quaternion.Euler(xRotation, 0, 0);
-        transform.Rotate(Vector3.up * mouseX);
+        gameCamera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        gameObject.transform.Rotate(Vector3.up * mouseX);
     }
 
 }
