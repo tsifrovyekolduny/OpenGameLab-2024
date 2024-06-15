26b6e9e1659ff927b51f83ec9d0a4dd1997df77a
Иван Вячеславович Нургазин <ivan.nurgazin@yandex.ru>
2024-06-16 00:50:43 +0700

    Начало работы над механикой рывков

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,11 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float moveSpeed;
-    [SerializeField] float mouseSensivity;
-    [SerializeField] float health;
+    [SerializeField] float moveSpeed = 5;
+    [SerializeField] float mouseSensivity = 10;
+    [SerializeField] float health = 100;
+    [SerializeField] float dashDistance = 30;
+    [SerializeField] float dashReloadTime = 1;
     [SerializeField] Camera gameCamera;
     [SerializeField] Vector3 position;
 
@@ -19,20 +21,56 @@ public class PlayerMovement : MonoBehaviour
     private PlayerInput _playerInput;
     private Vector2 _mouseMove;
     private float xRotation = 0f;
+    private bool _isDashReloading;
+    private bool _isDashing;
+    readonly private float _dashFrames = 30f;
+    readonly private float _dashTime = 0.2f;
 
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
         _playerInput = GetComponent<PlayerInput>();
         _playerInput.actions.Enable();
+        _isDashReloading = false;
+        _isDashing = false;
         Cursor.lockState = CursorLockMode.Locked;
-        health = 100f;
+    }
+
+    IEnumerator DashReload()
+    {
+
+        _isDashReloading = true;
+
+        yield return new WaitForSeconds(dashReloadTime);
+
+        _isDashReloading = false;
+    }
+
+    IEnumerator DashProcess(Vector2 moveDirection)
+    {
+        _isDashing = true;
+        _moveDirection = (moveDirection.y * transform.forward) + (moveDirection.x * transform.right);
+        float oneDashFrame = _dashTime / _dashFrames;
+        Debug.Log(oneDashFrame);
+        float moveInOneFrame = dashDistance * oneDashFrame;
+        float counter = _dashTime;
+        while (counter > 0) 
+        {
+            _characterController.Move(moveInOneFrame * _moveDirection);
+            counter -= oneDashFrame;
+            yield return new WaitForSeconds(oneDashFrame);
+        }
+        _isDashing = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+        if (!_isDashing)
+        {
+            Move();
+        }
+        
         LookAround();
     }
     
@@ -41,7 +79,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 moveDirection = _playerInput.actions.FindAction("Movement").ReadValue<Vector2>();
         _moveDirection = (moveDirection.y * transform.forward) + (moveDirection.x * transform.right);
-        _characterController.Move(_moveDirection * moveSpeed * Time.deltaTime);
+        _characterController.Move(moveSpeed * Time.deltaTime * _moveDirection);
     }
 
     public void LookAround()
@@ -58,4 +96,17 @@ public class PlayerMovement : MonoBehaviour
         gameObject.transform.Rotate(Vector3.up * mouseX);
     }
 
+    public void Dash(InputAction.CallbackContext context)
+    {
+        if (context.performed && !_isDashReloading)
+        {
+            Vector2 moveDirection = _playerInput.actions.FindAction("Movement").ReadValue<Vector2>();
+            StartCoroutine(DashProcess(moveDirection));
+            StartCoroutine(DashReload());
+            //Vector2 moveDirection = _playerInput.actions.FindAction("Movement").ReadValue<Vector2>();
+            //_moveDirection = (moveDirection.y * transform.forward) + (moveDirection.x * transform.right);
+            //_characterController.Move(dashDistance * _moveDirection);
+        }
+        
+    }
 }
